9d43115c3a92716f28340320f34a0bae15bf99ab
Aqsa Younis <aqsayounis2004@gmail.com>
2023-09-28 12:53:49 +0530

    Strings'

---
+++ src/main/kotlin/8StringMethods.kt
@@ -0,0 +1,17 @@
+// isEmpty()
+//uppercase()
+//lowercase()
+//equals()
+//trim()
+//length
+//plus() - concatenates
+
+fun main(args: Array<String>)
+{
+    var s1 = "  Hello"
+    var s2 =" world"
+    println(s1.equals(s2))
+
+    println(s1.trim())
+    println(s1.lowercase())
+}
