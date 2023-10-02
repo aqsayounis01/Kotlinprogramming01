672adbdc66778ebdb9663cc326526b6f65f4a65f
Aqsa Younis <aqsayounis2004@gmail.com>
2023-10-02 22:35:30 +0530

    map

---
+++ src/main/kotlin/21Maps.kt
@@ -0,0 +1,15 @@
+//map- array of key value pairs
+fun main(arg:Array<String>)
+{
+    var m = mapOf<Int,String>(2 to "code",3 to "program")   // cannot change it
+    println(m[2])
+    var mm = mutableMapOf<Int,String>(2 to "a",3 to "this")
+    mm.put(4 ,"changed ")
+    println(mm[4])
+
+
+    for ( (i,j) in mm)
+    {
+        println(mm[i]   )
+    }
+}
