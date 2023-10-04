6f07c9839b440e82610a6828c54ad2bb7cecdbc8
Aqsa Younis <aqsayounis2004@gmail.com>
2023-10-04 10:44:38 +0530

    nameC

---
+++ src/main/kotlin/0NmaingConvention.kt
@@ -0,0 +1,14 @@
+//convention is not important to follow
+//Classname -> start with an upper case letter
+//variable name -> camelCase
+//function name -> camelCase
+
+//kotlin is case sensitive language
+fun main(args:Array<String>)
+{
+    var hi=0
+    var Hi=9
+    println(hi)
+    println(Hi)
+
+}
