9abaaf2dfa08a23cdbdef6be3907218feab25aa0
Aqsa Younis <aqsayounis2004@gmail.com>
2023-10-04 10:23:24 +0530

    func

--- src/main/kotlin/3Functions.kt
+++ src/main/kotlin/3Functions.kt
@@ -1,10 +1,15 @@
+//Unit means void in kotlin (by default)
+//primitive functions - returns int , char, float etc
+//non primitive  - returns object, string etc
+// camelCase is used for function names
+
 fun main(args: Array<String>)
 {
    var name ="Aqsa Younis"
    display(name)
 }
 
-fun display(name:String)    // variable:Data type
+fun display(name:String):Unit    // variable:Data type
 {
     println(name)
 }
